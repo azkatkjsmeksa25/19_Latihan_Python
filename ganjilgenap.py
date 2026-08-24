a22bb8a350485ffa170858be078fa9436f25b807
19 M.Azka Itmanuniam Al Chaesaro <azka.tkjsmeksa25@gmail.com>
2026-08-24 14:53:03 +0700

    hallo

--- ganjilgenap.py
+++ ganjilgenap.py
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
-"ALAT PENGHITUNG GANJIL GENAP"
+# Program Menentukan Bilangan Ganjil atau Genap
+while True:
+    angka = int(input("Masukkan sebuah angka: "))
 
-"INPUT DATA ANGKA"
-X = int(input("Masukkan Angka : "))
-
-"INTI PROGRAM GANJIL GENAP"
-if X % 2 == 0:
-    print("Angka", X, "adalah Angka Genap")
-else:
-=======
-"ALAT PENGHITUNG GANJIL GENAP"
-
-"INPUT DATA ANGKA"
-X = int(input("Masukkan Angka : "))
-
-"INTI PROGRAM GANJIL GENAP"
-if X % 2 == 0:
-    print("Angka", X, "adalah Angka Genap")
-else:
->>>>>>> 4003c3d2e211309b83dce5af31737f2b299d9a25
-    print("Angka", X, "adalah Angka Ganjil")
+    if angka % 2 == 0:
+        print("Angka", angka, "adalah bilangan Genap")
+    else:
+        print("Angka", angka, "adalah bilangan Ganjil")
+    terus = str(input("Apakah Anda ingin memasukkan angka lagi? (y/n): "))
+    if terus == "y":
+        continue 
+    else:
+        print("Terima kasih telah menggunakan program ini.")
+        break  
