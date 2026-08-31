f2bfac4e244ac8cb5541c20b6e856870c4eb32d7
19 M.Azka Itmanuniam Al Chaesaro <azka.tkjsmeksa25@gmail.com>
2026-08-31 09:08:58 +0700

    Add files via upload

--- ganjilgenapnew.py
+++ ganjilgenapnew.py
@@ -1,6 +1,14 @@
-angka = int(input("Masukkan sebuah bilangan: "))
+while True:
+    # progam cek bialngan ganjilgenap
+    angka = int(input("Masukkan sebuah bilangan: "))
 
-if angka % 2 == 0:
-    print(angka, "adalah bilangan Genap")
-else:
-    print(angka, "adalah bilangan Ganjil")
+    if angka % 2 == 0:
+        print(angka, "adalah bilangan Genap")
+
+    else:
+        print(angka, "adalah bilangan Ganjil")
+
+    tombol = input("Tekan 'x' untuk keluar atau tekan tombol lain untuk melanjutkan: ")
+
+    if tombol == "x":
+        break
