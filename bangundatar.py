5b7f758467b1a4fb0688cb5f9ff1b0f224e907ad
19 M.Azka Itmanuniam Al Chaesaro <azka.tkjsmeksa25@gmail.com>
2026-09-21 09:09:19 +0700

    Add files via upload

--- bangundatar.py
+++ bangundatar.py
@@ -1,21 +1,12 @@
-def hitung_persegi():
-    print("--- Hitung Persegi ---")
-    sisi = float(input("Panjang sisi: "))
-
+def hitung_persegi(sisi):
     luas = sisi * sisi
     keliling = sisi + sisi + sisi + sisi
 
-    print(f"Luas persegi   : {luas}")
-    print(f"Keliling persegi: {keliling}")
+    return luas, keliling
 
 
-def hitung_persegi_panjang():
-    print("--- Hitung Persegi Panjang ---")
-    panjang = float(input("Panjang: "))
-    lebar = float(input("Lebar  : "))
-
+def hitung_persegi_panjang(panjang, lebar):
     luas = panjang * lebar
     keliling = 2 * panjang + 2 * lebar
 
-    print(f"Luas persegi panjang    : {luas}")
-    print(f"Keliling persegi panjang: {keliling}")
+    return luas, keliling
